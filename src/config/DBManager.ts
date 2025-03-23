ea896b7c4e35807b3f42ff3f92c9a87426a1c7e3
diegofmo0802 <diegofmo0802@gmail.com>
2025-03-23 17:19:19 +0000

    added database config

---
+++ src/config/DBManager.ts
@@ -0,0 +1,23 @@
+import Manager from "../DBManager/Manager";
+import * as Schemas from "./DBScheme";
+
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+
+if (!DB_HOST || !DB_PORT || !DB_USER || !DB_PASSWORD || !DB_NAME) throw new Error('missing environment variables');
+
+export const client = new Manager({
+    host: DB_HOST, port: parseInt(DB_PORT), 
+    username: DB_USER, password: DB_PASSWORD
+});
+
+export const db = client.dbSession(DB_NAME, Schemas);
+
+await db.operation(async (db) => {
+    await db.init(); return true;
+});
+
+export default db;
---
+++ src/config/DBScheme.ts
@@ -0,0 +1,16 @@
+import { Schema } from "../DBManager/Manager";
+
+const user = new Schema({
+    _id: { type: 'string', required: true, unique: true, minLength: 36, maxLength: 36 },
+    profile: { type: 'object', required: true, schema: {
+        firstName: { type: 'string', minLength: 2, maxLength: 20 },
+        lastName: { type: 'string', minLength: 2, maxLength: 20 },
+        username: { type: 'string', required: true, unique: true, minLength: 3, maxLength: 20 },
+        email: { type: 'string', required: true, unique: true, minLength: 6, maxLength: 100 }
+    } },
+    auth: { type: 'object', required: true, schema: {
+        verified: { type: 'boolean', required: true, default: false },
+        passwordHash: { type: 'string', required: true, minLength: 256, maxLength: 256, },
+        passwordSalt: { type: 'string', required: true, minLength: 32, maxLength: 32, }
+    } }
+});

7ff8483ee5f4a4b83af4604af797cff29d1a299a
diegofmo0802 <diegofmo0802@gmail.com>
2025-03-24 18:40:41 +0000

    changed db environments load

--- src/config/DBManager.ts
+++ src/config/DBManager.ts
@@ -1,17 +1,17 @@
 import Manager from "../DBManager/Manager.js";
 import * as Schemas from "./DBScheme.js";
 
-const DB_HOST = process.env.DB_HOST;
-const DB_PORT = process.env.DB_PORT;
-const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_NAME = process.env.DB_NAME;
-
-if (!DB_HOST || !DB_PORT || !DB_USER || !DB_PASSWORD || !DB_NAME) throw new Error('missing environment variables');
+const {
+    DB_HOST = 'localhost',
+    DB_PORT = '27017',
+    DB_USER = 'root',
+    DB_PASS = 'root',
+    DB_NAME = 'ds1-fp'
+} = process.env;
 
 export const client = new Manager({
     host: DB_HOST, port: parseInt(DB_PORT), 
-    username: DB_USER, password: DB_PASSWORD
+    username: DB_USER, password: DB_PASS
 });
 
 export const db = client.dbSession(DB_NAME, Schemas);

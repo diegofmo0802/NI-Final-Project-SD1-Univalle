742ad9f95d7e774c23a17c335f7d0d2c10fd9740
diegofmo0802 <diegofmo0802@gmail.com>
2025-04-02 23:31:18 +0000

    menu schema changed

--- client/logic/src/components/Menu.ts
+++ client/logic/src/components/Menu.ts
@@ -1,20 +1,32 @@
 import { Component, Element } from "../WebApp/WebApp.js";
+import Button from "./basic.components/Button.js";
+import Logo from "./basic.components/Logo.js";
+import Session from "./Session.js";
 
 export class Menu extends Component<'nav'> {
     protected component: Element<"nav">;
-    public buttons: Element<"button">[] = [];
-    public constructor() { super();
+    protected buttons: Button[] = [];
+    protected options: Element<'div'>;
+    public constructor(
+        public readonly session: Session,
+        public readonly logo: Logo
+    ) { super();
+        const sessionContainer = Element.new('div', null, { id: 'session' });
+        this.options = Element.new('div', null, { id: 'menu-options' });
         this.component = Element.new('nav', null, { id: 'menu' });
+
+        sessionContainer.append(session);
+        this.component.append(this.logo, this.options, sessionContainer);
     }
-    public addButton(eventName: string, text: string): Element<'button'> {
-        const button = Element.new('button', text);
-        button.on('click', (event) => this.dispatch(eventName, event));
+    public addButton(eventName: string, text: string): Button {
+        const button = new Button(text);
+        button.on('click', () => this.dispatch(eventName));
         this.buttons.push(button);
-        this.component.append(button);
+        this.options.append(button);
         return button;
     }
     public clean(): void {
         this.buttons = [];
-        this.component.clean();
+        this.options.clean();
     }
 }

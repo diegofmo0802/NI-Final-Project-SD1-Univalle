bed572921291949927804996dd0216d87eefa5d6
diegofmo0802 <diegofmo0802@gmail.com>
2025-04-03 01:21:03 +0000

    added endpoints to auth management

--- client/logic/src/api/Api.ts
+++ client/logic/src/api/Api.ts
@@ -1,8 +1,22 @@
-import _Auth from "./Auth";
-import _User from './User';
+import _Auth from "./Auth.js";
+import _User from './User.js';
 
 export namespace Api {
-    export import Auth = _Auth;
-    export import User = _User;
+    export import auth = _Auth;
+    export import user = _User;
+    export namespace Response {
+        export interface Base {
+            success: boolean;
+            code: number;
+        }
+        export interface Success<T extends any = any> extends Base {
+            success: true;
+            result: T;
+        }
+        export interface Error {
+            success: true;
+            reason: string;
+        }
+    }
 }
 export default Api;
--- client/logic/src/api/Auth.ts
+++ client/logic/src/api/Auth.ts
@@ -1,5 +1,33 @@
 export class Auth {
-
+    public static BASE_URL = '/api/auth';
+    public static async login(username: string, password: string): Promise<any> {
+        const response = await fetch(`${this.BASE_URL}/login`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', },
+            body: JSON.stringify({ username, password }),
+        });
+        return await response.json();
+    }
+    public static async register(username: string, email: string, password: string): Promise<any> {
+        const response = await fetch(`${this.BASE_URL}/register`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', },
+            body: JSON.stringify({ username, email, password }),
+        });
+        return await response.json();
+    }
+    public static async check() {
+        const response = await fetch(`${this.BASE_URL}/check`, {
+            method: 'GET',
+        });
+        return await response.json();
+    }
+    public static async logout() {
+        const response = await fetch(`${this.BASE_URL}/logout`, {
+            method: 'POST',
+        });
+        return await response.json();
+    }
 }
 export namespace Auth {
 

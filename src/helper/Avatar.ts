72734eb4f97e178dadb2ab6814e64e391da033af
diegofmo0802 <diegofmo0802@gmail.com>
2025-04-23 21:49:04 +0000

    added function save

--- src/helper/Avatar.ts
+++ src/helper/Avatar.ts
@@ -1,4 +1,6 @@
 import { randomUUID } from "crypto";
+import { promises as fs } from "fs";
+
 import { Image } from "./Image.js";
 
 export class Avatar {
@@ -31,6 +33,29 @@ export class Avatar {
         const avatarID = randomUUID();
         await Image.saveImage(`.data/avatar/${userID}/`, avatarID, data);
         return avatarID;
+    }
+    /**
+     * Get the avatar of the user
+     * @param userID - The id of the user
+     * @param avatarID - The id of the avatar
+     * @returns - The avatar of the user
+     */
+    public static async get(userID: string, avatarID: string): Promise<Buffer | null> {
+        const Path = Avatar.getPath(userID, avatarID);
+        if (!await this.exist(userID, avatarID)) return null
+        return fs.readFile(Path);
+    }
+    /**
+     * Check if the avatar exists
+     * @param userID - The id of the user
+     * @param avatarID - The id of the avatar
+     * @returns - True if the avatar exists, false otherwise
+     */
+    public static async exist(userID: string, avatarID: string): Promise<boolean> {
+        const Path = Avatar.getPath(userID, avatarID);
+        try { await fs.access(Path);
+            return true;
+        } catch(error) { return false; }
     }
 }
 export namespace Avatar {}

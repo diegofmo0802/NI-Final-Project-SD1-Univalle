b5a5cc6d4a3f23a2170de6b3a847b76d8f69b97e
diegofmo0802 <diegofmo0802@gmail.com>
2025-05-07 19:34:40 +0000

    implemented Request actions to api rules

--- src/apiRouter.ts
+++ src/apiRouter.ts
@@ -3,6 +3,7 @@ import ServerCore from "saml.servercore";
 import ApiRequest from "./helper/ApiRequest.js";
 import * as authRule from "./actions/auth.js";
 import * as userRule from "./actions/user.js";
+import * as requestRule from "./actions/requests.js";
 
 function route(rule: string): string {
     rule = rule.startsWith('/') ? rule.slice(1) : rule;
@@ -11,30 +12,46 @@ function route(rule: string): string {
 
 export function addApiRoutes(server: ServerCore) {
     // Auth rules/session
-    server.addAction('POST', route('auth/login'), (request, response) => {
+    server.addAction('POST', route('/api/auth/login'), (request, response) => {
         authRule.login(new ApiRequest(request, response));
     });
-    server.addAction('POST', route('auth/register'), (request, response) => {
+    server.addAction('POST', route('/api/auth/register'), (request, response) => {
         authRule.register(new ApiRequest(request, response));
     });
-    server.addAction('POST', route('auth/logout'), (request, response) => {
+    server.addAction('POST', route('/api/auth/logout'), (request, response) => {
         authRule.logout(new ApiRequest(request, response));
     });
-    server.addAction('GET', route('auth/check'), (request, response) => {
+    server.addAction('GET', route('/api/auth/check'), (request, response) => {
         authRule.checkSession(new ApiRequest(request, response));
     });
     // User rules/Profile
-    server.addAction('GET', route('user/$uuid/avatar/$avatarID'), (request, response) => {
-        userRule.getAvatar(new ApiRequest(request, response));
-    });
-    server.addAction('GET', route('user/$uuid'), (request, response) => {
-        userRule.getUser(new ApiRequest(request, response));
-    });
     server.addAction('GET', '/api/user/', (request, response) => {
         userRule.getUsers(new ApiRequest(request, response));
     });
-    server.addAction('POST', route('user/$uuid'), (request, response) => {
+    server.addAction('GET', route('api/user/$uuid'), (request, response) => {
+        userRule.getUser(new ApiRequest(request, response));
+    });
+    server.addAction('GET', route('api/user/$uuid/avatar/$avatarID'), (request, response) => {
+        userRule.getAvatar(new ApiRequest(request, response));
+    });
+    server.addAction('POST', route('/api/user/$uuid'), (request, response) => {
         userRule.editUser(new ApiRequest(request, response));
+    });
+    // Request rules
+    server.addAction('GET', '/api/user/$uuid/postulations', (request, response) => {
+        requestRule.getUserPostulations(new ApiRequest(request, response));
+    });
+    server.addAction('GET', '/api/user/$uuid/requests', (request, response) => {
+        requestRule.getUserRequests(new ApiRequest(request, response));
+    });
+    server.addAction('GET', route('/api/request/$uuid'), (request, response) => {
+        requestRule.getRequest(new ApiRequest(request, response));
+    });
+    server.addAction('POST', '/api/user/$uuid/request', (request, response) => {
+        requestRule.createRequest(new ApiRequest(request, response));
+    });
+    server.addAction('GET', route('/api/request/$uuid/postulations'), (request, response) => {
+        requestRule.getUserPostulations(new ApiRequest(request, response));
     });
     // All other routes
     server.addAction('GET', route('*'), (request, response) => {

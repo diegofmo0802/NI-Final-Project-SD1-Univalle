92990a348de85278b92db500b3cb785e0ceb265e
diegofmo0802 <diegofmo0802@gmail.com>
2025-05-08 05:27:35 +0000

    added button request

--- client/logic/src/config.ts
+++ client/logic/src/config.ts
@@ -16,6 +16,7 @@ const menu = new Menu(session, new Logo('DS1 Final', '/client/assets/logo.svg'))
 menu.addButton('home', Language.get('menu.button.home'));
 menu.addButton('about', Language.get('menu.button.about'));
 menu.addButton('users', Language.get('menu.button.users'));
+menu.addButton('requests', Language.get('menu.button.requests'));
 
 const content = Element.new('div', null, { id: 'content' });
 

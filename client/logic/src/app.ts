6e6542d2b07eaabfefc8a505d706a31714b03293
diegofmo0802 <diegofmo0802@gmail.com>
2025-05-08 06:20:23 +0000

    fix: session is not part of schema

--- client/logic/src/app.ts
+++ client/logic/src/app.ts
@@ -12,7 +12,7 @@ export const loading = new Loading('/client/assets/logo.svg');
 export const session = new Session();
 export const menu = new Menu(session, new Logo('DS1 Final', '/client/assets/logo.svg'));
 export const content = Element.new('div', null, { id: 'content' });
-export const schema = [menu, session];
+export const schema = [menu, content];
 export const components = { menu, session, content };
 
 menu.addButton('home', Language.get('menu.button.home'));
